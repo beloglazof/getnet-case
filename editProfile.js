75d68fed1727f718f93b770e7761800b4aa77950
beloglazof <supsupno@gmail.com>
2018-09-11 23:51:12 +0500

    clear

--- editProfile.js
+++
@@ -1,5 +0,0 @@
-function editProfile(userData, userEmail) {
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.open("GET", "updateDB.php?q=" + userData + "&email=" + userEmail, true);
-  xmlhttp.send();
-}
--- userData.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $session_id = $_SESSION['id'];
-    
-    
-    $result = $connection -> query("SELECT * FROM users WHERE user_id = $session_id");
-    $user_data = $result -> fetch_assoc();
-    
-    
-?>
